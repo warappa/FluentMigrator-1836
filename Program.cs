3fb370a42796461906e0656e7a24c024dd4ed2a5
warappa <spoiltnr1@gmx.at>
2024-07-18 21:51:58 +0200

    Add TryAdd example

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using FluentMigrator.Runner;
 using FluentMigrator.Runner.VersionTableInfo;
 using FluentMigrator;
 using System.Data.SQLite;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("FluentMigrator 1836 repo");
+Console.WriteLine("---------------------");
 
 const string databaseFile = "test1836.db";
 const string connectionString = $"Data Source={databaseFile}";
@@ -17,8 +18,17 @@ if (File.Exists(databaseFile))
 
 var services = new ServiceCollection();
 
-// wrong
-services.AddScoped<IVersionTableMetaData, CustomVersionMetaData>();
+Console.WriteLine("Repo of undocumented but mandatory DI registration order (before or after 'AddFluentMigratorCore()').");
+Console.WriteLine("Should register custom VersionInfo table too early (wrong)? (Enter 'y' for 'yes', any key for 'no'):");
+var registerTooEarly = Console.ReadKey().Key == ConsoleKey.Y;
+Console.WriteLine();
+
+// 1836 registration issue repo
+if (registerTooEarly)
+{
+    // wrong - registered too early
+    services.AddScoped<IVersionTableMetaData, CustomVersionMetaData>();
+}
 
 services.AddFluentMigratorCore()
     .ConfigureRunner(x =>
@@ -28,11 +38,27 @@ services.AddFluentMigratorCore()
             .ScanIn(typeof(Program).Assembly).For.Migrations();
     });
 
-// right
-//services.AddScoped<IVersionTableMetaData, CustomVersionMetaData>();
+if (!registerTooEarly)
+{
+    // right - registered after "AddFluentMigratorCore()"
+    services.AddScoped<IVersionTableMetaData, CustomVersionMetaData>();
+}
+
+
+// "Replace"-question demo
+// Replacing a service is the brute force way to ensure an implementation is used.
+// Adding an implementation only if non is yet found is more graceful.
+// Here's a demo of only registering a service if it is not yet registered in service collection.
+// Add first service - in *user's* code.
+services.AddScoped<ISomeLibraryInterface, UserImplementation>();
+
+// A library (like FluentMigrator) only tries to add a default implementation if it was not yet registered before.
+// This way it does not matter if user registers before or after the libary because user's registration takes priority.
+services.TryAddScoped<ISomeLibraryInterface, DefaultLibraryImplementation>();
 
 var serviceProvider = services.BuildServiceProvider();
 
+// (Custom)VersionInfo table repo
 using (var scope = serviceProvider.CreateScope())
 {
     var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
@@ -48,8 +74,22 @@ using (var connection = new SQLiteConnection(connectionString))
 
     var customVersionInfoExists = TableExists(command, "CustomVersionInfo");
 
-    Console.WriteLine($"Default VersionInfo table: {defaultVersionInfoExists}");
-    Console.WriteLine($"Custom VersionInfo table: {customVersionInfoExists}");
+    Console.WriteLine($"Registered user override too early: {registerTooEarly}");
+    Console.WriteLine($"Default VersionInfo table found (wrong): {defaultVersionInfoExists}");
+    Console.WriteLine($"Custom VersionInfo table found (right): {customVersionInfoExists}");
+}
+
+// DI registration demo
+using (var scope = serviceProvider.CreateScope())
+{
+    var test = scope.ServiceProvider.GetRequiredService<ISomeLibraryInterface>();
+    
+    Console.WriteLine();
+    Console.WriteLine("---------------------");
+    Console.WriteLine("Give user DI registration priority");
+    Console.WriteLine("Demo of giving user registered services priority by using 'TryAdd...' in library code.");
+    Console.WriteLine("Resolved value should be 'UserImplementation', although registered before library's registrations.");
+    Console.WriteLine($"Resolved instance type: {test.GetType().Name}");
 }
 
 static bool TableExists(SQLiteCommand command, string tableName)
@@ -78,4 +118,18 @@ public class AddLogTable : Migration
     {
         Delete.Table("Log");
     }
+}
+
+public interface ISomeLibraryInterface
+{
+}
+
+public class DefaultLibraryImplementation : ISomeLibraryInterface
+{
+
+}
+
+public class UserImplementation : ISomeLibraryInterface
+{
+
 }
